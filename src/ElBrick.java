01a628f3a4ffcb4c206d179fad01d7e0c70d8a1e
tyron <tyron@MSI>
2021-01-30 17:37:57 -0600

    /*  * This class will house the ElBrick of the Tetris Game  */

---
+++ src/ElBrick.java
@@ -0,0 +1,81 @@
+/*
+ * This class will house the ElBrick of the Tetris Game
+ */
+/**
+ * Class for the ElBrick
+ * for each unique brick
+ * @version 1.000
+ * @author Tyron Corpuz & Vanessa Ezenduka
+ * 11/19/2020
+ */
+
+import java.awt.*;
+
+public class ElBrick extends TetrisBrick
+{
+    public ElBrick(Color color, int orient, int numSeg, int col){
+        
+        super(color, orient, numSeg, col);
+        initPosition(col);
+    }
+    
+    @Override
+    public int[][] initPosition(int cols)
+    {
+        int referenceCol = cols/2;
+        int position[][] = {{referenceCol-1,1}, {referenceCol,1}, {referenceCol+1,1}, {referenceCol+1,0}};
+        return position;
+    }
+    
+    public void rotate()
+    {
+        if(orientation == 0)
+        {
+            position[0][0] = position[0][0] + 1;
+            position[0][1] = position[0][1] + 1;
+            position[2][0] = position[2][0] - 1;
+            position[2][1] = position[2][1] - 1;
+            position[3][0] = position[3][0] - 2;
+            position[3][1] = position[3][1];
+            orientation    = 1;
+        }
+        else if(orientation == 1)
+        {
+            position[0][0] = position[0][0] + 1;
+            position[0][1] = position[0][1] - 1;
+            position[2][0] = position[2][0] - 1;
+            position[2][1] = position[2][1] + 1;
+            position[3][0] = position[3][0] + 2;
+            position[3][1] = position[3][1];
+            orientation    = 2;
+        }
+        else if(orientation == 2)
+        {
+            position[0][0] = position[0][0] - 1;
+            position[0][1] = position[0][1] - 1;
+            position[2][0] = position[2][0] + 1;
+            position[2][1] = position[2][1] + 1;
+            position[3][0] = position[3][0];
+            position[3][1] = position[3][1] + 2;
+            orientation    = 3;
+        }
+        else if(orientation == 3)
+        {
+            position[0][0] = position[0][0] - 1;
+            position[0][1] = position[0][1] + 1;
+            position[2][0] = position[2][0] + 1;
+            position[2][1] = position[2][1] - 1;
+            position[3][0] = position[3][0];
+            position[3][1] = position[3][1] - 2;
+            orientation    = 0;
+        }
+    }
+    
+    @Override
+    public void unrotate()
+    {
+        rotate();
+        rotate();
+        rotate();
+    }
+}
